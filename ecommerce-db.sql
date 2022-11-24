94b92cdfce03ab0f1ab5c4e210c8e542d5b45f16
tiendat86 <dangtiendat86@gmail.com>
2022-11-24 19:47:04 +0700

    update database

---
+++ ecommerce-db.sql
@@ -0,0 +1,218 @@
+CREATE DATABASE  IF NOT EXISTS `ecom` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ecom`;
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ecom
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bill`
+--
+
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bill` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `total_price` double NOT NULL,
+  `user_id` bigint DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKqhq5aolak9ku5x5mx11cpjad9` (`user_id`),
+  CONSTRAINT `FKqhq5aolak9ku5x5mx11cpjad9` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill`
+--
+
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+INSERT INTO `bill` VALUES (3,'2022-11-21 17:25:25','2022-11-21 17:25:25',61670000,2,'UNPAYMENT');
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `brand`
+--
+
+DROP TABLE IF EXISTS `brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `brand` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brand`
+--
+
+LOCK TABLES `brand` WRITE;
+/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+INSERT INTO `brand` VALUES (1,'2022-11-19 21:45:53','2022-11-19 21:45:53','Apple'),(2,'2022-11-19 21:46:10','2022-11-19 21:46:10','Samsung'),(3,'2022-11-19 21:46:15','2022-11-19 21:46:15','Lg'),(4,'2022-11-19 21:46:20','2022-11-19 21:46:20','Sony'),(5,'2022-11-19 21:46:24','2022-11-19 21:46:24','Xiaomi'),(6,'2022-11-19 21:46:41','2022-11-19 21:46:41','MSI'),(7,'2022-11-19 21:46:45','2022-11-19 21:46:45','Asus'),(8,'2022-11-19 21:46:56','2022-11-19 21:46:56','HP'),(9,'2022-11-19 21:47:02','2022-11-19 21:47:02','Lenovo'),(10,'2022-11-19 21:47:16','2022-11-19 21:47:16','Oppo'),(11,'2022-11-19 21:47:24','2022-11-19 21:47:24','Realme'),(12,'2022-11-19 21:47:28','2022-11-19 21:47:28','Nokia');
+/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKl70asp4l4w0jmbm1tqyofho4o` (`user_id`),
+  CONSTRAINT `FKl70asp4l4w0jmbm1tqyofho4o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` VALUES (1,NULL,NULL,1),(2,NULL,NULL,2),(3,NULL,NULL,3);
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `description` longtext,
+  `inventory_number` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `brand_id` bigint DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKs6cydsualtsrprvlf2bb3lcam` (`brand_id`),
+  CONSTRAINT `FKs6cydsualtsrprvlf2bb3lcam` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'2022-11-19 22:00:27','2022-11-22 09:38:51','Apple đã chính thức trình làng bộ 3 siêu phẩm iPhone 11, trong đó phiên bản iPhone 11 64GB có mức giá rẻ nhất nhưng vẫn được nâng cấp mạnh mẽ như iPhone Xr ra mắt trước đó.',100,'Iphone 11',11690000,'SMARTPHONE',1,'http://res.cloudinary.com/dmiqsmeab/image/upload/v1669084730/1_product_Iphone%2011.jpg'),(2,'2022-11-19 22:01:54','2022-11-19 22:01:54','Samsung Galaxy Z Flip4 128GB đã chính thức ra mắt thị trường công nghệ, đánh dấu sự trở lại của Samsung trên con đường định hướng người dùng về sự tiện lợi trên những chiếc điện thoại gập. Với độ bền được gia tăng cùng kiểu thiết kế đẹp mắt giúp Flip4 trở thành một trong những tâm điểm sáng giá cho nửa cuối năm 2022.',100,'Samsung Galaxy Z Flip4',20690000,'SMARTPHONE',2,NULL),(3,'2022-11-19 22:02:55','2022-11-19 22:02:55','Galaxy S22 Ultra 5G chiếc smartphone cao cấp nhất trong bộ 3 Galaxy S22 series mà Samsung đã cho ra mắt. Tích hợp bút S Pen hoàn hảo trong thân máy, trang bị vi xử lý mạnh mẽ cho các tác vụ sử dụng vô cùng mượt mà và nổi bật hơn với cụm camera không viền độc đáo mang đậm dấu ấn riêng.',100,'Samsung Galaxy S22 Ultra',24990000,'SMARTPHONE',2,NULL),(4,'2022-11-19 22:03:36','2022-11-19 22:03:36','iPhone 13 Pro Max 256GB - siêu phẩm mang trên mình bộ vi xử lý Apple A15 Bionic hàng đầu, màn hình Super Retina XDR 120 Hz, cụm camera khẩu độ f/1.5 cực lớn, tất cả sẽ mang lại cho bạn những trải nghiệm tuyệt vời chưa từng có.',100,'iPhone 13 Pro Max',30990000,'SMARTPHONE',1,NULL),(5,'2022-11-22 10:12:52','2022-11-22 10:12:55','Samsung Galaxy A13 6GB ra mắt với một thiết kế đầy trẻ trung và năng động, cùng với đó là hiệu năng ổn định giúp bạn có được những trải nghiệm mượt mà cho các công việc lướt web, xem phim, nghe nhạc hay chơi được các tựa game hiện hành.',10,'Samsung Galaxy A13',46900000,'SMARTPHONE',NULL,'http://res.cloudinary.com/dmiqsmeab/image/upload/v1669086774/5_product_Samsung%20Galaxy%20A13.jpg');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_in_cart`
+--
+
+DROP TABLE IF EXISTS `product_in_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_in_cart` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `cart_id` bigint DEFAULT NULL,
+  `product_id` bigint DEFAULT NULL,
+  `bill_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKq5fiqoevrmlirjqjxs0kvlnqq` (`cart_id`),
+  KEY `FKmbb9ii8dfmagbt971nh30ikcm` (`product_id`),
+  KEY `FKashwo2cx8wffg6hmidq95xcqx` (`bill_id`),
+  CONSTRAINT `FKashwo2cx8wffg6hmidq95xcqx` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`id`),
+  CONSTRAINT `FKmbb9ii8dfmagbt971nh30ikcm` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  CONSTRAINT `FKq5fiqoevrmlirjqjxs0kvlnqq` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_in_cart`
+--
+
+LOCK TABLES `product_in_cart` WRITE;
+/*!40000 ALTER TABLE `product_in_cart` DISABLE KEYS */;
+INSERT INTO `product_in_cart` VALUES (4,'2022-11-20 00:28:41','2022-11-21 17:25:25',1,2,1,3),(5,'2022-11-20 00:29:12','2022-11-20 00:29:12',1,1,1,NULL),(6,'2022-11-21 14:11:11','2022-11-21 17:25:25',2,2,3,3),(7,'2022-11-21 14:20:03','2022-11-21 14:20:16',2,2,4,NULL);
+/*!40000 ALTER TABLE `product_in_cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `user_status` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `verify_code` varchar(64) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`),
+  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'2022-11-19 20:40:07','2022-11-19 20:40:28','dangtiendat86@gmail.com',NULL,'tiendat86','$2a$10$8deeZWrOdQ/NHVn0bOJ4/.rd3vwMceJXM6Pqra6392R/NaMAsY.uK','CUSTOMER','ACTIVE','tiendat86','VvA26QK1xETskQWkRqscCoohg8L5fnT5jhHUWMEm0XtL1TR1xz7Iy8JZVWIYIA84',NULL),(2,'2022-11-19 20:42:17','2022-11-19 20:42:17','admin@gmail.com',NULL,'admin','$2a$10$s19JnpQUcY78/bpw/jaCeuj4VonsEFxWOXuXw67sMm0l6FvnhQNxG','CUSTOMER|MANAGER','ACTIVE','admin','PNxYYd4eyugz3WMpH32FFh1YtBe642voLG8Ldghb1BwlZSCfWwRprUdmJmta5GRr',NULL),(3,'2022-11-19 20:42:51','2022-11-19 20:42:51','customer@gmail.com',NULL,'customer','$2a$10$1bwlUdVlAmehKxgR32yxvu0iEsvG1n/KykUFn5iXPp.oVVq2j.QX2','CUSTOMER','customer','customer','kXw4OhKkgF1Zw2QvciZ6dXq2px2d46zjKW5cmCbpuCxVsyL0ddDbwjWgwCsIp88m',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-24 19:45:36
